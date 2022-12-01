14bcf5e25e1063b2776f68af18d798dddcae272c
Solomon Kebede <solomonaau188@gmail.com>
2022-12-01 15:11:00 +0000

    Fix/Add server response parser function

--- main2.py
+++ main2.py
@@ -19,12 +19,13 @@ from sys import argv
 
 import hashlib
 
+import time
+
 logging.basicConfig(
     level=logging.ERROR,
     format=' %(asctime)s -%(levelname)s - %(message)s'
     )
-
-# To take input from the terminal.
+'''# To take input from the terminal.
 image_paths = []
 user_inputs = [str(x) for x in input("Please enter image path(s): ").split(", ")]
 
@@ -34,10 +35,9 @@ for user_input in user_inputs:
             print("Image(s) being processed...")
         image_path = image_paths.append(user_input)
     else:
-        print("Path not found, please enter another path")
-    
+        print("Path not found, please enter another path")'''    
 
-"""image_paths = [
+image_paths = [
 "./test_images/everything-has-beauty-confucius-quote.jpg",
 "./test_images/maya-angelou-famous-quote.webp",
 "./test_images/download2.jpg",
@@ -45,14 +45,13 @@ for user_input in user_inputs:
 "./test_images/d760e79a7997acefb79c917b7771d75f.jpg",
 "./test_images/03ab9fbff391a8a3de0569050745a555.jpg",
 "./test_images/download.png",
-]"""
-
-"""image_paths = ["./images_test_2/download.jpeg",
+"./test_images/2101793258-amharic-poem.jpg",
+"./images_test_2/download.jpeg",
 './images_test_2/my-family-i-love-them-cinderheart-lionblaze-you-only-stepped-on-a-thorn-lionblaze-help-im-dyinblaze.jpeg',
 './images_test_2/Variations-in-handwriting-style-random-sample-of-handwriting-taken-from-IRONOFF-database.png',
 './images_test_2/random-text-to-wrap-around-picture.jpg',
 './images_test_2/preview-image-onlinefiletools.png',
-]"""
+]
 
 base_url = "https://lens.google.com"
 endpoint_1 = f"{base_url}/_/upload/"
@@ -85,6 +84,26 @@ async def post(session, url, _headers, _data=None, request_type = None):
                 logging.error(f'Status Code_2: {response.status}')
                 return None
 
+def parse_results(response):
+    c = 0
+    all_lists = []
+    result_1 = response.split('\n')[3]
+    result_2 = json.loads(result_1)[0][2]
+    for i in result_2.split("["):
+        for j in i.split("]"):
+            if j != '' and j not in all_lists:
+                potential_list = (f"{j}")
+                all_lists.append(potential_list)
+                c += 1
+    for x in all_lists:
+        if x[0] == ',' and 'Acna4G' in x:
+            ocr_result_index = all_lists.index(x) - 1
+            ocr_result = all_lists[ocr_result_index]
+            logging.info(f"[{ocr_result_index}] => {ocr_result}")
+    ocr_results = json.loads(f"[{ocr_result}]")
+    return ocr_results
+    
+
 async def main():
     async with aiohttp.ClientSession() as session:
         results_endpoint1 = (
@@ -93,8 +112,9 @@ async def main():
             post(session, endpoint_1,
                 header('upload_url'),
                 request_type="headers-only") for image_path in image_paths]
-        )
             )
+        )
+
         filepath = get_path(image_paths)
         endpoint_2_awaitables = []
         endpoint_3_awaitables = []
@@ -130,8 +150,8 @@ async def main():
             gp_url_encoded = res_2_json["payload"]
             gp_url_decoded = base64.b64decode(gp_url_encoded)
             gp_url = f"https://{gp_url_decoded.split(b'https://')[1].split(b'(')[0].decode('utf-8')}"
-
             logging.info(f"[3.0] {gp_url}\n")
+
             # Get OCR data
             results_url = url4batchexecute()
             data = data4batchexecute(gp_url)
@@ -145,22 +165,7 @@ async def main():
         # print(results_endpoint3)
         for r in results_endpoint3:
             # Parsing for the result
-            result_1 = r.split('\n')[3]
-            result_2 = json.loads(result_1)[0][2]
-            result_3 = repr(result_2)
-
-            c = 0
-            all_lists = []
-
-            for i in result_2.split("["):
-                for j in i.split("]"):
-                    if j != '':
-                        potential_list = (f"{j}")
-                        all_lists.append(potential_list)
-                        c+=1
-
-            ocr_result = all_lists[-45]
-            ocr_results = json.loads(f"[{ocr_result}]")
+            ocr_results = parse_results(r)
             logging.info(f"[4.0] {ocr_results}\n")
 
             print("Final Result:")
@@ -169,4 +174,6 @@ async def main():
             print()
 
 if __name__ == '__main__':
+    start_time = time.time()
     asyncio.run(main())
+    print(f'{time.time() - start_time}s')
